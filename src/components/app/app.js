e6890d4a8a1799a4f6a573f5dbf308f014189140
Dmitry <DmitryYamshikov@gmail.com>
2021-04-11 13:32:30 +0700

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AppHeader from '../app-header/';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
+
+const App = ()=>{
+    return(
+        <div className="app">
+            <AppHeader />
+            <div className="search-panel d-flex">
+                <SearchPanel />
+                <PostStatusFilter />
+            </div>
+            <div>
+                <PostList/>
+            </div>
+            <PostAddForm/>
+            
+        </div>
+    )
+};
+export default App;
+
+
+//! rafce
+/* const app = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default app */
