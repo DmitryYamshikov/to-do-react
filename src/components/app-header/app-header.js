3292d4f5c2dae00e1cccdedfce679daf72f20992
Dmitry <DmitryYamshikov@gmail.com>
2021-04-15 00:01:58 +0700

    Добавил функции лайк и Избранное

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './app-header.css';
 
 
-const AppHeader = ()=> {
+const AppHeader = ({liked, counter})=> {
     return (
         <div className='app-header d-flex'>
             <h1>Дмитрий Ямщиков</h1>
-            <h2>5 записей, из них понравилось 0</h2>
+            <h2>{counter} записей, из них понравилось {liked}</h2>
         </div>
     )
 }
